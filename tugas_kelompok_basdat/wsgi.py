dad9a0fcad7b6010de7028daf3dfe8dbb706c3d3
Hsyadid <hubban.syadid@ui.ac.id>
2025-05-28 21:30:13 +0700

    setup deploy 3

--- tugas_kelompok_basdat/wsgi.py
+++ tugas_kelompok_basdat/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tugas_kelompok_basdat.settings')
 
 application = get_wsgi_application()
+app = application  # add this for Vercel
